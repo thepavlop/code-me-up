223553b92b0d9ec4a6087760ffac35326f9e247c
thepavlop <47693506+thepavlop@users.noreply.github.com>
2019-03-23 14:39:03 +0200

    improved foramtting to shapes.py

--- shapes.py
+++ shapes.py
@@ -1,28 +1,37 @@
-shape = input(" enter a shape ")
+"""
+This program calculates the area and perimeter of a given shape.
+Shapes: 'rectangle', 'triangle'.
+"""
 
+shape = input("Enter a shape (rectangle/triangle): ")
+
+# User input is 'rectangle': 
 if shape == 'rectangle':
-    i1 = input ('enter an int ')
-    i1= int(i1)
-    i2= input ('enter an int ')
-    i2= int (i2)
+    # Ask user for a height and width of the rectangle
+    i1 = input ('Enter the width of the rectangle: ')
+    i1 = int(i1)
+    i2 = input ('Enter the height of the rectangle: ')
+    i2 = int (i2)
+    # Check that the height and the width are positive
     while i1 < 0:
-        i1 = int(input('enter an int '))
+        i1 = int(input('Enter the width of the rectangle: '))
     while i2 < 0:
-        i2 = int(input('enter an int '))
+        i2 = int(input('Enter the height of the rectangle: '))
+    # Calculate area and perimeter:
     x = i1 * i2
-    y= i1+i1+i2+i2
-    print ('the area is:', x)
-    print ('the perimeter is:', y)
+    y = i1 + i1 + i2 + i2
+    print ('The area of the rectangle is:', x)
+    print ('The perimeter of the rectangle is:', y)
 
 
 
     elif shape == 'triangle':
-    i3 = input ("enter an int ")
-    i3= int(i3)
-    i4 = input ("enter an int ")
-    i4= int (i4)
-    i5=input ("enter an int ")
-    i5= int (i5)
+        i3 = input ("enter an int ")
+        i3= int(i3)
+        i4 = input ("enter an int ")
+        i4= int (i4)
+        i5=input ("enter an int ")
+        i5= int (i5)
     while i3 < 0:
         i3 = int(input('enter an int '))
     while i4 < 0:
