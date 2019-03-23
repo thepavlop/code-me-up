a9c2f570cca2dfba57ee0ac55435158e3d4654ba
thepavlop <47693506+thepavlop@users.noreply.github.com>
2019-03-23 14:40:20 +0200

    uploaded more files

---
+++ random.py
@@ -0,0 +1,13 @@
+x= input("please enter an int")
+x= int(x)
+my_list = list(range (0,x+1,3))
+print(my_list[-1])
+
+
+
+
+
+
+
+
+
---
+++ synartisi.py
@@ -0,0 +1,12 @@
+def synartisi (x,y,z):
+    result = (x-y)**2 - (z/x)* (y-3)
+    return result
+print (synartisi(2,4,6))
+print (synartisi(4,2,6))
+print (synartisi(6,4,2))
+print (synartisi(2,6,4))
+print (synartisi(6,2,4))
+print (synartisi(6,4,2))
+print (synartisi(4,6,2))
+
+
